0b931d22dd13e794742d9bb77ab530ea5fb7f4ad
Olivier Mengué <dolmen@cpan.org>
2025-04-20 11:41:49 +0200

    sub/play: add more comments documenting the /compile API
    Add curl examples to document interaction with
    https://play.golang.org/compile API.

--- sub/play/doc.go
+++ sub/play/doc.go
@@ -1,2 +1,14 @@
 // Command play is the sub command launched by "goeval -play".
+//
+// play sends the program code to the Go Playground at https://play.golang.org/compile
+// and replays the received events respecting event delays.
+//
+//	$ curl -s -X POST --data-urlencode body@- https://play.golang.org/compile <<EOF
+//	package main
+//	import "fmt"
+//	func main() {
+//	  fmt.Println("Hello, world!")
+//	}
+//	EOF
+//	{"Errors":"","Events":[{"Message":"Hello, world!\n","Kind":"stdout","Delay":0}],"Status":0,"IsTest":false,"TestsFailed":0}
 package main
