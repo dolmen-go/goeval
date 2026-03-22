a48817e5c9b58a01dd5f5d0fbc2b08312264d136
Olivier Mengué <dolmen@cpan.org>
2026-03-22 13:44:30 +0100

    sub/play: add Example_compileError
    For more coverage.

--- sub/play/play_test.go
+++ sub/play/play_test.go
@@ -19,6 +19,7 @@
 package main_test
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
@@ -46,4 +47,24 @@ func Example_time() {
 
 	// Output:
 	// 2009-11-10T23:00:00Z
+}
+
+func Example_compileError() {
+	cmd := exec.Command("go", "run", "play.go", userAgent)
+	cmd.Env = append(os.Environ(), "GO111MODULE=off")
+	cmd.Stdin = strings.NewReader(`@`)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout // We want to capture stderr, so redirect stderr to the Example's stdout
+	err := cmd.Run()
+	if err, ok := err.(*exec.ExitError); ok {
+		if err.ExitCode() != 1 {
+			fmt.Println("Status 1 expected, got ", err.ExitCode())
+		}
+	}
+
+	// Note: the output check is fragile as it relies on the Go compiler output which might change.
+
+	// Output:
+	// prog.go:1:1: illegal character U+0040 '@'
+	// exit status 1
 }
