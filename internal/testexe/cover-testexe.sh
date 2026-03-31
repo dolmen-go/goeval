4136170bc46998104ca6e287c90bf75c4638a143
Olivier Mengué <dolmen@cpan.org>
2026-03-31 14:37:16 +0200

    internal/testexe/cover-testexe.sh: improve coverage measurement

--- internal/testexe/cover-testexe.sh
+++ internal/testexe/cover-testexe.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+cd "$(dirname "$0")" || exit 1
+
 rm -rf .coverage.1 .coverage.2 .coverage
 mkdir .coverage.1 .coverage.2 .coverage
-go test -cover -coverpkg ./... -parallel=10 -race ./echo -args -test.gocoverdir="$(pwd)"/.coverage.1
-GOCOVERDIR=$(pwd)/.coverage.2 go test ./echo -args
+go test -cover -coverpkg ./... -parallel=10 -race -args -test.gocoverdir="$(pwd)"/.coverage.1
+GOCOVERDIR=$(pwd)/.coverage.2 go test ./...
 go tool covdata merge -pcombine -i .coverage.1,.coverage.2 -o .coverage
 go tool covdata textfmt -i .coverage -o .coverage.out
+rm -rf .coverage.1 .coverage.2 .coverage
 
 test -t 0 && go tool cover -html=.coverage.out
 
