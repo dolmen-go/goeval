9f7e7cd0c97d730f861b6923547a430d0478f5ad
Olivier Mengué <dolmen@cpan.org>
2026-05-04 00:17:03 +0200

    internal/playmock: stricter route checks
    More checks of Content-Type, charset.
    Add limit on request body size.

--- internal/playmock/playmock.go
+++ internal/playmock/playmock.go
@@ -21,8 +21,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"mime"
 	"net"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -67,26 +69,41 @@ func (s *Server) Run(ctx context.Context) (u string, cleanup func(), _ error) {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /compile", func(w http.ResponseWriter, r *http.Request) {
-		if s.Compile == nil {
-			http.Error(w, "Not Implemented", http.StatusNotImplemented)
+		contentType := r.Header.Get("Content-Type")
+		mediaType, params, err := mime.ParseMediaType(contentType)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotAcceptable)
 			return
 		}
-
 		var req CompileRequest
-		contentType := r.Header.Get("Content-Type")
-		if contentType == "application/json" {
-			if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		switch mediaType {
+		case "application/json":
+			if len(params) > 0 {
+				if charset, ok := params["charset"]; ok && charset != "utf-8" {
+					http.Error(w, "Invalid charset", http.StatusNotAcceptable)
+					return
+				}
+			}
+			dec := json.NewDecoder(&io.LimitedReader{R: r.Body, N: 4096})
+			if err := dec.Decode(&req); err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
-		} else {
-			// application/x-www-form-urlencoded
+		case "application/x-www-form-urlencoded":
 			if err := r.ParseForm(); err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
 			req.Body = r.FormValue("body")
 			req.WithVet = r.FormValue("withVet") == "true"
+		default:
+			http.Error(w, "Unexpected media type", http.StatusNotAcceptable)
+			return
+		}
+
+		if s.Compile == nil {
+			http.Error(w, "Not Implemented", http.StatusNotImplemented)
+			return
 		}
 
 		resp, err := s.Compile(&req)
@@ -97,18 +114,32 @@ func (s *Server) Run(ctx context.Context) (u string, cleanup func(), _ error) {
 
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(resp)
 	})
 
 	mux.HandleFunc("POST /share", func(w http.ResponseWriter, r *http.Request) {
-		if s.Share == nil {
-			http.Error(w, "Not Implemented", http.StatusNotImplemented)
+		contentType := r.Header.Get("Content-Type")
+		mediaType, params, err := mime.ParseMediaType(contentType)
+		if err != nil || (!strings.HasPrefix(mediaType, "application/") && !strings.HasPrefix(mediaType, "text/")) {
+			http.Error(w, err.Error(), http.StatusNotAcceptable)
+			return
+		}
+		if len(params) > 0 {
+			if charset, ok := params["charset"]; ok && charset != "utf-8" {
+				http.Error(w, "Invalid charset", http.StatusNotAcceptable)
+				return
+			}
+		}
+
+		body, err := io.ReadAll(&io.LimitedReader{R: r.Body, N: 4096})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		if s.Share == nil {
+			http.Error(w, "Not Implemented", http.StatusNotImplemented)
 			return
 		}
 
@@ -120,6 +151,7 @@ func (s *Server) Run(ctx context.Context) (u string, cleanup func(), _ error) {
 
 		w.Header().Set("Content-Type", "text/plain")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(resp.ID))
 	})
 
